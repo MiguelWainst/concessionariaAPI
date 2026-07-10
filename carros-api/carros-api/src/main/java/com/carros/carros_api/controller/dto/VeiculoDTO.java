b5d5eaea4f9b0024fb079ee8560816bddd6cf391
Miguel Wainstein <miguel.wainst@gmail.com>
2026-07-10 17:02:50 -0300

    cirando veiculoDTO.

---
+++ carros-api/carros-api/src/main/java/com/carros/carros_api/controller/dto/VeiculoDTO.java
@@ -0,0 +1,26 @@
+package com.carros.carros_api.controller.dto;
+
+import com.carros.carros_api.entity.CategoriaVeiculo;
+import com.carros.carros_api.entity.Montadora;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record VeiculoDTO(
+        UUID id,
+        @NotBlank(message = "O chassi é obrigatório.")
+        String chassi,
+        @NotBlank(message = "O modelo é obrigatório.")
+        String modelo,
+        @NotBlank(message = "A data de fabricação é obrigatória.")
+        String dataFabricacao,
+        CategoriaVeiculo categoria,
+        @Max(value = 14, message = "Valor inválido, muito grande.")
+        BigDecimal preco,
+        @NotNull(message = "A montadora é obrigatória.")
+        MontadoraDTO montadora
+) {
+}
